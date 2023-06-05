63e0a66358a68edc5b8b78e1d3cb2b0defd4239b
sasha-filitovich <sasha.filitovich@mail.ru>
2023-06-05 21:54:19 +0300

    feat: add reviews file

---
+++ reviews.js
@@ -0,0 +1,141 @@
+const reviews = [
+  {
+    headline: 'Perfect',
+    mainPart:
+      'Perfectly clean apartment, feels new, friendly and welcoming owner, very good breakfast. Both kitchen and bathroom were quite small in our apartment, there could be more equipment in kitchen (e.g. could find only forks and spoons, but no dining knives).',
+    name: 'Jakub, Czech',
+    date: 'March 1, 2023',
+    mark: '9',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart: 'Everything, really perfect',
+    name: 'Birgit, German',
+    date: 'February 19, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Perfect accomodation! Great location, the stuff is professional, kind and helpful. FREE SAUNA. Breakfast was absolutely fantastic! Great selection, all replenished several times and all excellent.',
+    name: 'Pavla, Czech',
+    date: 'February 16, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Perfect',
+    mainPart:
+      'vriendelijke eigenaar, vertelde ons over te bezoeken locaties, restaurants en was zeer behulpzaam. Prachtig appartement, net nieuw.',
+    name: 'Marga, Netherlands',
+    date: 'February 7, 2023',
+    mark: '9',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Very clean, nice apartment. Hosts were super friendly and hospitable. Good location, quiet area, just a short walk to the center of the village. Everything inside looks very new and taken care of. Ski and snowboard storage room. Great breakfast.',
+    name: 'Rafael, German',
+    date: 'February 6, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'sehr feiner Aufenthalt in einer sehr gut gelegenen Pension mit äußerst aufmerksamem Chef.',
+    name: 'Anton, Austria',
+    date: 'February 4, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Assolutamente da consigliare! accoglienza ottima,la.struttura moderna,vicino al centro.Da tornare! I proprietari Marina e Alex sono davvero delle persone squisite,ci hanno dato ottimo consigli su qualsiasi cosa.Siamo stati accolti come in una famiglia ,persone veramente di buon cuore.La colazione è ottima,le camere moderne e pulite con ogni comfort, comodissimo al centro,alle piste.Ce davvero tanto da vedere anche per chi non scia.Tutto perfetto!',
+    name: 'Diana, Italy',
+    date: 'January 29, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Great breakfast - very comfortable beds, and above all: very friendly and helpful staff. Would definitily go again. All was great - only people I would not recommend the appartment for are disabled people or people who do not like stairs - there is no elevator and you need to use teh stairs to reach every room.',
+    name: 'Thomas, Austria',
+    date: 'January 22, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Perfect',
+    mainPart: 'The owner is very kind and helpful to whatever you require.',
+    name: 'Brian, Malta',
+    date: 'January 14, 2023',
+    mark: '9',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart: 'Odličan smještaj. Svakom bih preporučila. Izvanredno osoblje.',
+    name: 'Mikela, Croatia',
+    date: 'January 8, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Great breakfast. Kind hosts. Big, comfortable and clean apartment. Walking distance to center.',
+    name: 'Irena, Croatia',
+    date: 'January 4, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart: 'Great host, breakfast and location!',
+    name: 'Matej, Croatia',
+    date: 'January 2, 2023',
+    mark: '10',
+  },
+  {
+    headline: 'Great',
+    mainPart: 'odlicna lokacija, prijetne sobe in zelo gostoljubna lastnika. obvezno se pridemo.',
+    name: 'Mateja, Slovenia',
+    date: 'December 31, 2022',
+    mark: '10',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Zelo prijazno osebje. Za zajtrk ti je ponujeno ogromno razlicnih stvari. Cistoca na nivoju.',
+    name: 'Anja, Slovenia',
+    date: 'December 11, 2022',
+    mark: '10',
+  },
+  {
+    headline: 'Perfect',
+    mainPart:
+      'Super moderne, sauber und frisch renovierte Unterkunft. Es war alles da, was ich mir hätte wünschen können. Sehr leckeres Frühstück. Unser Gastgeber hat uns sogar angeboten, ein Brötchen für unterwegs zu schmieren, da die Geschäfte am Tag unserer Abfahrt geschlossen hatten. Zusätzlich hat er uns viele hilfreiche Tipps über Aktivitäten in der Umgebung gegeben.',
+    name: 'Alexandra, German',
+    date: 'November 21, 2022',
+    mark: '9',
+  },
+  {
+    headline: 'Fabulous',
+    mainPart:
+      'Pri Marusi is very close to the center, it has nice view, cosy, modern and clean rooms, the owners are very kind and helpful, providing a good breakfast and a lot of information about see sights and restaurants. Thanks for everything!',
+    name: 'Kriszta, Hungary',
+    date: 'November 6, 2022',
+    mark: '10',
+  },
+  {
+    headline: 'Perfect',
+    mainPart:
+      'Haben uns sehr wohl gefühlt. Der Gastgeber ist toll, hat uns viele Tipps für Ausflüge gegeben und ein super Frühstück gezaubert. Guter Start für viele Wanderungen! Parkplätze sind vorhanden.',
+    name: 'Sandra, German',
+    date: 'October 13, 2022',
+    mark: '9',
+  },
+  {
+    headline: 'Perfect',
+    mainPart:
+      'Angenehme Unterkunft für kurzen oder mehrtägigen Aufenthalt. Modern eingerichtete Zimmer mit guter Ausstattung. Freundliche und hilfsbereite Gastgeber. Gute, fußläufige Lage. Gute, reichhaltiges Frühstück.',
+    name: 'Marko, German',
+    date: 'October 7, 2022',
+    mark: '9',
+  },
+];
